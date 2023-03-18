c352d370f1221cb65837c475c77475140aa4f1b3
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-18 19:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
-function palindrome(str){
+ const string = str.toLowerCase();
+ const len = str.length;
 
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
